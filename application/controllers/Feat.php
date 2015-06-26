95b14c507a47ed702b4a5ddd89eaa6c38488e3cc
Rajat Saxena <rajat.saxena035@gmail.com>
2015-06-27 02:28:27 +0530

    Update Feat.php
    Made a function for Opportunity.php page.

--- application/controllers/Feat.php
+++ application/controllers/Feat.php
@@ -3,29 +3,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Feat extends CI_Controller {
 		
-		public function vnb()
+	public function vnb()
 	{
-       $this->load->view('pages/link');
+       		$this->load->view('pages/link');
 		$this->load->view('pages/header');
 		$this->load->view('pages/virtual_notice_board');
 		$this->load->view('pages/extras');
 		$this->load->view('pages/footer');
 		 
 	}
+	
         public function events()
 	{
-        $this->load->view('pages/link');
+        	$this->load->view('pages/link');
 		$this->load->view('pages/header');
 		$this->load->view('pages/events');
 		$this->load->view('pages/extras');
 		$this->load->view('pages/footer');
 		
 	}	
-	    public function exams()
+	
+	public function exams()
 	{
-        $this->load->view('pages/link');
+        	$this->load->view('pages/link');
 		$this->load->view('pages/header');
 		$this->load->view('pages/exams/py_papers');
+		$this->load->view('pages/extras');
+		$this->load->view('pages/footer');
+	}
+	
+	public function opportunities()
+	{
+        	$this->load->view('pages/link');
+		$this->load->view('pages/header');
+		$this->load->view('pages/opportunities');
 		$this->load->view('pages/extras');
 		$this->load->view('pages/footer');
 	}
