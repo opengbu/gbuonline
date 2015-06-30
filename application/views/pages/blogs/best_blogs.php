aec4e4debed148c84bc422af67966b33fe9f3496
Rajat Saxena <rajat.saxena035@gmail.com>
2015-07-01 02:56:21 +0530

    Update best_blogs.php

--- application/views/pages/blogs/best_blogs.php
+++ application/views/pages/blogs/best_blogs.php
@@ -1,4 +1,5 @@
 
+
    
 <div class="page-header" style = "background-color: darkred; padding-top: 10px; padding-left: 50px; margin-top: 15px;">
             <font color="white"  face="Monotype Corsiva">
@@ -43,9 +44,30 @@
     What if the Robots defeat... <a href="#">Read More</a><br>
     </p>    
   </div>
-   <div class="panel-footer"><b>Aman Verma - 14/ICS/005</b>
+   <div class="panel-footer">
+   
+	<div class="row">
+	
+	<div class="col-md-4">
+    
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> &nbsp;<span class="badge">15</span></button>&nbsp;
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span> &nbsp;<span class="badge">4</span></button>&nbsp;
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> &nbsp;<span class="badge">9</span></button>
+	</div>
+    
+	
+	
+   
+   <div class="col-md-8" style="text-align: right;  padding-top: 5px;">
+   <b>Aman Verma - 14/ICS/005</b>
    </div>
-</div>
+   
+  
+   </div>
+   </div>
+   
+   </div>
+
    
 
   
@@ -63,51 +85,141 @@
     What if the Robots defeat... <a href="#">Read More</a><br>
     </p>    
   </div>
-   <div class="panel-footer"><b>Aman Verma - 14/ICS/005</b>
+   <div class="panel-footer">
+   
+	<div class="row">
+	
+	<div class="col-md-4">
+    
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> &nbsp;<span class="badge">15</span></button>&nbsp;
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span> &nbsp;<span class="badge">4</span></button>&nbsp;
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> &nbsp;<span class="badge">9</span></button>
+	</div>
+    
+	
+	
+   
+   <div class="col-md-8" style="text-align: right;  padding-top: 5px;">
+   <b>Aman Verma - 14/ICS/005</b>
    </div>
-</div>
-    
-    
-    <div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title"><b>Artificial Intelligence - The next step towards human destruction</b></h3>
-  </div>
-  <div class="panel-body">
-    <p align="justify">
-    Artificial Intelligence is one of the hot topics of the current decade. All 
-    the major research nowadays is on AI. The computer scientists all over the world are
-    inventing new algorithms that would help robots take its own decisions and learn from its past.
-    But a little is paid towards the destruction it would cause once its actually implemented.
-    What if the Robots defeat... <a href="#">Read More</a><br>
-    </p>    
-  </div>
-   <div class="panel-footer"><b>Aman Verma - 14/ICS/005</b>
+   
+  
    </div>
-</div>
-    
-    <div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title"><b>Artificial Intelligence - The next step towards human destruction</b></h3>
-  </div>
-  <div class="panel-body">
-    <p align="justify">
-    Artificial Intelligence is one of the hot topics of the current decade. All 
-    the major research nowadays is on AI. The computer scientists all over the world are
-    inventing new algorithms that would help robots take its own decisions and learn from its past.
-    But a little is paid towards the destruction it would cause once its actually implemented.
-    What if the Robots defeat... <a href="#">Read More</a><br>
-    </p>    
-  </div>
-   <div class="panel-footer"><b>Aman Verma - 14/ICS/005</b>
    </div>
-</div>
-    
-    <hr>
-    
-    
+   
+   </div>
 
+   
+
+  
+    
+    
+  <div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><b>Artificial Intelligence - The next step towards human destruction</b></h3>
+  </div>
+  <div class="panel-body">
+    <p align="justify">
+    Artificial Intelligence is one of the hot topics of the current decade. All 
+    the major research nowadays is on AI. The computer scientists all over the world are
+    inventing new algorithms that would help robots take its own decisions and learn from its past.
+    But a little is paid towards the destruction it would cause once its actually implemented.
+    What if the Robots defeat... <a href="#">Read More</a><br>
+    </p>    
+  </div>
+   <div class="panel-footer">
+   
+	<div class="row">
+	
+	<div class="col-md-4">
+    
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> &nbsp;<span class="badge">15</span></button>&nbsp;
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span> &nbsp;<span class="badge">4</span></button>&nbsp;
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> &nbsp;<span class="badge">9</span></button>
+	</div>
+    
+	
+	
+   
+   <div class="col-md-8" style="text-align: right;  padding-top: 5px;">
+   <b>Aman Verma - 14/ICS/005</b>
+   </div>
+   
+  
+   </div>
+   </div>
+   
+   </div>
+
+   
+
+    
+  <div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><b>Artificial Intelligence - The next step towards human destruction</b></h3>
+  </div>
+  <div class="panel-body">
+    <p align="justify">
+    Artificial Intelligence is one of the hot topics of the current decade. All 
+    the major research nowadays is on AI. The computer scientists all over the world are
+    inventing new algorithms that would help robots take its own decisions and learn from its past.
+    But a little is paid towards the destruction it would cause once its actually implemented.
+    What if the Robots defeat... <a href="#">Read More</a><br>
+    </p>    
+  </div>
+   <div class="panel-footer">
+   
+	<div class="row">
+	
+	<div class="col-md-4">
+    
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> &nbsp;<span class="badge">15</span></button>&nbsp;
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span> &nbsp;<span class="badge">4</span></button>&nbsp;
+	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> &nbsp;<span class="badge">9</span></button>
+	</div>
+    
+	
+	
+   
+   <div class="col-md-8" style="text-align: right;  padding-top: 5px;">
+   <b>Aman Verma - 14/ICS/005</b>
+   </div>
+   
+  
+   </div>
+   </div>
+   
+   </div>
 
         </div>
 
 
- 
+ <!--Modal-->
+ <div class="modal fade blogs" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Login to GBU Online</h4>
+      </div>
+      <div class="modal-body">
+        <div class="input-group">
+			<span class="input-group-addon" id="basic-addon1">Enter</span>
+				<input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+		</div>
+		<br>
+		<div class="input-group">		
+		<span class="input-group-addon" id="basic-addon1">Enter</span>
+			<input type="password" class="form-control" placeholder="Password" aria-describedby="basic-addon1">
+		</div>
+		
+      </div>
+      <div class="modal-footer">
+	  <center>
+        <button type="button" class="btn btn-success">Proceed</button>
+        </center>
+      </div>
+    </div>
+  </div>
+</div>
+              
