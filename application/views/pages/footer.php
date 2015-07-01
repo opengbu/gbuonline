66d4fe38d42cbcb916c29b76a4486681edad6e72
gargsarthak30 <gargsarthak30@gmail.com>
2015-07-01 18:28:55 +0530

    Update footer.php

--- application/views/pages/footer.php
+++ application/views/pages/footer.php
@@ -1,161 +1,236 @@
 <div class="row">
 
-<style>
+    <style>
 
-footer {
-  background:url("<?php echo base_url('resources/images/footer1.jpg')?>");
-  margin: 0px;
-  padding-left: 270px;
-  width : 100%;
-    }
-</style>
+        footer {
+            background:url("<?php echo base_url('resources/images/footer1.jpg') ?>");
+            margin: 0px;
+            padding-left: 270px; 
+            width : 100%;
+        }
+    </style>
 
 
-<footer class="footer" role="contentinfo">
-<br>
+    <footer class="footer" role="contentinfo">
+        <br>
 
-    <div class="wrapper">
+        <div class="wrapper">
             <div class="row" >
                 <div class="col-md-3">
 
 
 
 
-<button type="button" class="btn btn-block" data-toggle="modal" data-target=".bs-example-modal-lg-1">Team</button>
+                    <button type="button" class="btn btn-block" data-toggle="modal" data-target=".bs-example-modal-lg-1">Team</button>
 
-    <div class="modal fade bs-example-modal-lg-1" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-               <center>
-                   <hr>
-                   We are a group of 5 students working under guidance of <br><b>Dr. Amit K. Awasthi.</b> <hr>
-                </center>
-                <center>
-                <ul>
-                    <i>
-                        <div class="row">
-                            <div class="col-sm-4">
-                                <img src="<?php echo base_url('resources/images/varun.jpg')?>" height ="120" width="120"><br>VARUN GARG (Login/Admin Panel)
+                    <div class="modal fade bs-example-modal-lg-1" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <center> 
+                                    <hr>
+                                    We are a group of 5 students working under guidance of <br><b>Dr. Amit K. Awasthi.</b> <hr>
+                                </center>
+                                <center>     
+                                    <ul>
+                                        <i>
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <img src="<?php echo base_url('resources/images/varun.jpg') ?>" height ="120" width="120"><br><b>VARUN GARG </b>(Admin Panel)
+                                                </div>
+                                                <div class="col-sm-4">
+                                                    <img src="<?php echo base_url('resources/images/shobhit.jpg') ?>" height ="120" width="120"><br><b>SHOBHIT SHARMA</b> (Hostel Management System)
+                                                </div>
+
+                                                <div class="col-sm-4">
+                                                    <img src="<?php echo base_url('resources/images/bhawesh.jpg') ?>" height ="120" width="120"><br><b>BHAWESH CHANDOLA</b> (Back End Database Management)
+
+                                                </div>
+                                            </div>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-sm-6">
+                                                    <img src="<?php echo base_url('resources/images/sarthak.jpg') ?>" height ="120" width="120"><br><b>SARTHAK GARG</b> (Front End Designer)<br>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <img src="<?php echo base_url('resources/images/rajat.jpg') ?>" height ="120" width="120"><br><b>RAJAT SAXENA</b> (Front End Designer)<br>
+                                                </div>
+                                            </div>
+
+
+                                        </i>      
+
+                                    </ul>
+                                    <hr>
+                                </center>
                             </div>
-                            <div class="col-sm-4">
-                                <img src="<?php echo base_url('resources/images/shobhit.jpg')?>" height ="120" width="120"><br>SHOBHIT SHARMA (Hostel Management System)
-                            </div>
+                        </div>
+                    </div>
 
-                            <div class="col-sm-4">
-                                <img src="<?php echo base_url('resources/images/bhawesh.jpg')?>" height ="120" width="120"><br>BHAWESH CHANDOLA (Back End Database Management)
-
-                            </div>
-                            </div>
-                        <hr>
-                            <div class="row">
-                            <div class="col-sm-6">
-                                <img src="<?php echo base_url('resources/images/sarthak.jpg')?>" height ="120" width="120"><br>SARTHAK GARG (Front End Designer)<br>
-                            </div>
-                                <div class="col-sm-6">
-                                <img src="<?php echo base_url('resources/images/rajat.jpg')?>" height ="120" width="120"><br>RAJAT SAXENA (Front End Designer)<br>
-                            </div>
-                            </div>
-
-
-                    </i>
-
-                </ul>
-                    <hr>
-            </center>
-            </div>
-        </div>
-    </div>
-
-<br>
-<br>
-<button type="button" class="btn btn-block" data-toggle="modal" data-target=".bs-example-modal-lg-2">Vision</button>
-
-    <div class="modal fade bs-example-modal-lg-2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-               <center>
-                   <hr/>
-                &nbsp; * Our mission is to take GBU to next level.<br>
-                &nbsp; * Help students to get all information on a single portal.<br>
-                &nbsp; * Digitalize GBU<br>
-                    <hr/>
-               </center>
-
-            </div>
-        </div>
-    </div>
-
-
-</div>
-
-
-<div class="col-md-3">
-
-<button type="button" class="btn btn-block" data-toggle="modal" data-target=".bs-example-modal-lg-3">Google Groups</button>
-
-    <div class="modal fade bs-example-modal-lg-3" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <center>
                     <br>
-                    &nbsp; * <a href="https://groups.google.com/forum/#!forum/gbu-cs-102-2013">GBU CS 102 2013</a><hr>
-                    &nbsp; * <a href="https://groups.google.com/forum/#!forum/gbu-cs-2014">GBU CS 2014</a><hr>
-                 &nbsp; * <a href="https://groups.google.com/forum/#!forum/gbu-ec-2014-19">GBU EC 2014-19</a><hr>
-                  &nbsp; * <a href="https://groups.google.com/forum/#!forum/gbu-mba-2014">GBU MBA 2014</a><hr>
-                &nbsp; * <a href="https://groups.google.com/forum/#!forum/gbuec2013">GBU EC 2013</a><hr>
-                &nbsp; * <a href="https://groups.google.com/forum/#!forum/gbucs2012">GBU CS 2012</a><hr>
-                &nbsp; * <a href="https://groups.google.com/forum/#!forum/ict_softb">GBU Computer Science 2011-16</a><hr>
-                &nbsp; * <a href="https://groups.google.com/forum/#!forum/civil-2014-gbu">civil 2014-gbu</a><br><hr>
-                </center>
+                    <br>
+                    <button type="button" class="btn btn-block" data-toggle="modal" data-target=".bs-example-modal-lg-2">Vision</button>
+
+                    <div class="modal fade bs-example-modal-lg-2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <center> 
+                                    <hr/>
+                                    &nbsp;GBU Online is built with the vision
+                                    of helping students and teachers to get all information about the university on a 
+                                    single platform. Now there's no need to rush over the whole  511 acre campus for any information. All you need is an internet connection
+                                    and everything is at your device's screen. Be it exam papers, information regarding any event or your hostel/mess related activities, GBU Online is Packed with everything.
+                                    <hr/>
+                                </center>
+
+                            </div>
                         </div>
-        </div>
-    </div>
-<br>
-<br>
-<button type="button" class="btn btn-block" formaction="http://www.math.gbuonline.in">Maths Site</button>
-</div>
-
-      <div class="col-md-3">
-
-<button type="button" class="btn btn-block" data-toggle="modal" data-target=".bs-example-modal-lg-5">Contact Us</button>
-
-    <div class="modal fade bs-example-modal-lg-5" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <center>
-                &nbsp; * <b>Varun Garg - 9868176400<b><br>
-                &nbsp; * <b>Rajat Saxena - 9013211136<b><br>
-                &nbsp; * <b>Sarthak Garg - 9999981051<b><br>
-                &nbsp; * <b>Shobhit Sharma - 9953282606<b><br>
-                &nbsp; * <b>Bhawesh Chandola - 99990971678<b><br>
-                </center>
-                        </div>
-        </div>
-    </div>
-<br>
-<br>
-
-<button type="button" class="btn btn-block" onclick="location.href='<?php echo site_url('feat/opportunities')?>'">Opportunities</button>
+                    </div>
 
 
-
-</div>
                 </div>
-</div>
-      <br>
-      <br>
 
-<div  style="margin-left:325px" class="row">
-    <div class="col-md-12">
-        <p>
-            <b>&copy; kNight_Coders</b>
-        </p>
 
-    </div>
+                <div class="col-md-3">
 
-</div>
+                    <button type="button" class="btn btn-block" data-toggle="modal" data-target=".bs-example-modal-lg-3">Google Groups</button>
 
-</footer>
+                    <div class="modal fade bs-example-modal-lg-3" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content ">
+                                <center>
+                                    <br>
+                                    <div class="list-group">
+                                        &nbsp;  <a href="https://groups.google.com/forum/#!forum/ict_softb" class="list-group-item">Computer Science 2011-16</a>
+                                        &nbsp;  <a href="https://groups.google.com/forum/#!forum/gbucs2012" class="list-group-item">Computer Science 2012-17</a>
+                                        &nbsp;  <a href="https://groups.google.com/forum/#!forum/gbu-cs-102-2013" class="list-group-item">Computer Science 2013-18</a>
+                                        &nbsp;  <a href="https://groups.google.com/forum/#!forum/gbuec2013" class="list-group-item">Electronics 2013-18</a>
+                                        &nbsp;  <a href="https://groups.google.com/forum/#!forum/gbu-cs-2014" class="list-group-item">Computer Science 2014-19</a>
+                                        &nbsp;  <a href="https://groups.google.com/forum/#!forum/gbu-ec-2014-19" class="list-group-item">Electronics 2014-19</a>
+                                        &nbsp;  <a href="https://groups.google.com/forum/#!forum/civil-2014-gbu" class="list-group-item">Civil 2014-19</a>
+                                        &nbsp;  <a href="https://groups.google.com/forum/#!forum/gbu-mba-2014" class="list-group-item">MBA 2014</a>       
+                                    </div>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+                    <button type="button" class="btn btn-block" onclick = "location.href = 'http://www.math.gbuonline.in';">Maths Site</button>
+                </div>
+
+                <div class="col-md-3">
+
+                    <button type="button" class="btn btn-block" data-toggle="modal" data-target=".bs-example-modal-lg-5">Connect With Us</button>
+
+                    <div class="modal fade bs-example-modal-lg-5" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+
+                                <center>
+                                    <h2>Team : kNight_Coders</h2>
+                                </center>
+
+                                <div class="media">
+                                    <div class="media-left">
+
+                                        <img class="media-object" src="<?php echo base_url('resources/images/varun.jpg') ?>" height="90" width="90">
+
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Varun Garg</h4>
+                                        <h6>Stack Overflow Profile: <a href = "https://careers.stackoverflow.com/varun-garg">https://careers.stackoverflow.com/varun-garg</a></h6>
+                                        <h6>Email: varun.10@live.com</h6>
+                                        <h6>Phone No: +919868176400</h6>
+
+                                    </div>
+                                </div>
+
+                                <div class="media">
+                                    <div class="media-left media-middle">
+
+                                        <img class="media-object" src="<?php echo base_url('resources/images/rajat.jpg') ?>" height="90" width="90">
+
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Rajat Saxena</h4>
+                                        <h6>LinkedIn Profile: <a href="https://in.linkedin.com/in/rajat24">https://in.linkedin.com/in/rajat24</a></h6>
+                                        <h6>Email: rajat.saxena035@gmail.com</h6>
+                                        <h6>Phone No: +919013211136</h6>
+                                    </div>
+                                </div>
+                                <div class="media">
+                                    <div class="media-left media-middle">
+
+                                        <img class="media-object" src="<?php echo base_url('resources/images/sarthak.jpg') ?>" height="90" width="90">
+
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Sarthak Garg</h4>
+                                        <h6>LinkedIn Profile: <a href="https://in.linkedin.com/in/gargsarthak">https://in.linkedin.com/in/gargsarthak</a></h6>
+                                        <h6>Email: garg_sarthak@yahoo.com</h6>
+                                        <h6>Phone No: +919999981051</h6>
+                                    </div>
+                                </div>
+                                <div class="media">
+                                    <div class="media-left media-middle">
+
+                                        <img class="media-object" src="<?php echo base_url('resources/images/shobhit.jpg') ?>" height="90" width="90">
+
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Shobhit Sharma</h4>
+                                        <h6>LinkedIn Profile: <a href="https://in.linkedin.com/pub/shobhit-sharma/7b/776/467">https://in.linkedin.com/pub/shobhit-sharma/7b/776/467</a></h6>
+                                        <h6>Email: shobhit95sharma@gmail.com</h6>
+                                        <h6>Phone No: +919953282606</h6>
+                                    </div>
+                                </div>
+                                <div class="media">
+                                    <div class="media-left media-middle">
+
+                                        <img class="media-object" src="<?php echo base_url('resources/images/bhawesh.jpg') ?>" height="90" width="90">
+
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Bhawesh Chandola</h4>
+                                        <h6>LinkedIn Profile: <a href = "https://in.linkedin.com/pub/bhawesh-chandola/7b/894/bba">https://in.linkedin.com/pub/bhawesh-chandola/7b/894/bba</a></h6>
+                                        <h6>Email: bhawesh.chandola@gmail.com</h6>
+                                        <h6>Phone No: +918860132317</h6>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+                    <button type="button" class="btn btn-block" data-toggle="modal" data-target=".bs-example-modal-sm-6">Admin Login</button>
+
+                    <div class="modal fade bs-example-modal-sm-6" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-sm">
+                            <div class="modal-content">
+                                <a href="#"><center>Administrator Login</center></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>   
+            </div>
+        </div>
+        <br>
+        <br>
+
+        <div  style="margin-left:325px" class="row">
+            <div class="col-md-12">
+                <p>
+                    <b>&copy; kNight_Coders</b>
+                </p>
+
+            </div>
+
+        </div>
+
+    </footer>
 </div>
 </div>
 </body>
