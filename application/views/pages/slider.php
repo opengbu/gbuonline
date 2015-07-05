1f496e595c3ce9dc02bb68e44b060e5335faf099
Sarthak Garg <gargsarthak30@gmail.com>
2015-07-05 09:38:22 +0530

    Update slider.php
    Slider optimized.

--- application/views/pages/slider.php
+++ application/views/pages/slider.php
@@ -5,13 +5,13 @@
      <img src="<?php echo base_url('resources/images/image3.jpg')?>" height="500"/>
      <img src="<?php echo base_url('resources/images/image4.jpg')?>" height="500"/>
     
-     <img src="<?php echo base_url('resources/images/image6.jpg')?>" height="500"/>
-     <img src="<?php echo base_url('resources/images/image7.jpg')?>" height="500"/>
-     <img src="<?php echo base_url('resources/images/image8.jpg')?>" height="500"/>          
+     <!--<img src="<?php echo base_url('resources/images/image6.jpg')?>" height="500"/>-->
+    <!-- <img src="<?php echo base_url('resources/images/image7.jpg')?>" height="500"/>
+     <img src="<?php echo base_url('resources/images/image8.jpg')?>" height="500"/>-->          
      
-     <img src="<?php echo base_url('resources/images/image10.jpg')?>" height="500"/>
+     <!--<img src="<?php echo base_url('resources/images/image10.jpg')?>" height="500"/>-->
      <img src="<?php echo base_url('resources/images/image11.jpg')?>" height="500"/>
-     <img src="<?php echo base_url('resources/images/image12.jpg')?>" height="500"/>
+     <!--<img src="<?php echo base_url('resources/images/image12.jpg')?>" height="500"/>-->
      <!--<img src="<?php echo base_url('resources/images/image13.jpg')?>" height="500"/>
      <img src="<?php echo base_url('resources/images/image14.jpg')?>" height="500"/>-->                                  
  </div>    
