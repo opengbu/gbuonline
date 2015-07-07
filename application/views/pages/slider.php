cccfec5edd43f70c844e03ec5fea2e4d6132cce7
Sarthak Garg <gargsarthak30@gmail.com>
2015-07-07 19:07:20 +0530

    Update slider.php

--- application/views/pages/slider.php
+++ application/views/pages/slider.php
@@ -1,4 +1,5 @@
 <div class="row">
+     <div class="container">
 <div class="slider">
      <img src="<?php echo base_url('resources/images/image1.jpg')?>" height="500"/>
      <img src="<?php echo base_url('resources/images/image2.jpg')?>" height="500"/>
@@ -14,6 +15,7 @@
      <!--<img src="<?php echo base_url('resources/images/image12.jpg')?>" height="500"/>-->
      <!--<img src="<?php echo base_url('resources/images/image13.jpg')?>" height="500"/>
      <img src="<?php echo base_url('resources/images/image14.jpg')?>" height="500"/>-->                                  
- </div>    
+ </div> 
+ </div>
 </div>
  
