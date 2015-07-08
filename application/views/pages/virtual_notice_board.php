66d8336064e922ca4029f8aad3d823962663f896
Rajat Saxena <rajat.saxena035@gmail.com>
2015-07-09 04:48:35 +0530

    Loops in vnb

--- application/views/pages/virtual_notice_board.php
+++ application/views/pages/virtual_notice_board.php
@@ -1,144 +1,35 @@
 
-
-     
+<!--Virtual Notice Board-->
 <div class="container-fluid" style=" margin-right: 10px; margin-left: 10px;" >
-     <div class="row">
-    
-	<div class="col-md-9" style="padding-right:30px; border-right: 2px solid #ccc;">        
+     <div class="row">    
+		<div class="col-md-9" style="padding-right:30px; border-right: 2px solid #ccc;">        
+			<div class="list-group">
+				
+				<!--Loop Starts-->
+				
+				<?php
+					for($x=0;$x<8;$x++)
+					{
+				?>
+							<a href="#" class="list-group-item">
+								<div class="row">
+									<div class="col-md-8">
+										<h4 class="list-group-item-heading">Change in the Grading scheme</h4>
+										<p class="list-group-item-text"><b>Posted By:</b> Dr. Vishal Sharma</p>
+									</div>
+									<div class="col-md-4" style="padding-top: 10px;">
+										<center>
+											<span class="badge" > 25 - 05 - 2015 </span>    
+										</center>
+									</div>
+								</div>
+							</a>
+							<br>
+				<?php
+					}
+				?>
+				
+				<!--Loop Ends-->
  
-<div class="list-group">
-  <a href="#" class="list-group-item">
-      <div class="row">
-          <div class="col-md-8">
-    <h4 class="list-group-item-heading">Bar Council of India Approval Letter</h4>
-    <p class="list-group-item-text"><b>Posted By:</b> Dr. Vishal Sharma</p>
-    </div>
-          <div class="col-md-4" style="padding-top: 10px;">
-                <center>
-                    <span class="badge" > 25 - 05 - 2015 </span>    
-                </center>
-          </div>
-          </div>
-  </a>
-    <br>
- 
-     <a href="#" class="list-group-item">
-    <div class="row">
-          <div class="col-md-8">
-    <h4 class="list-group-item-heading">Fellowship for Gate qualified students</h4>
-    <p class="list-group-item-text"><b>Posted By:</b> Dr. Vishal Sharma</p>
-    </div>
-          <div class="col-md-4" style="padding-top: 10px;">
-                <center>
-                    <span class="badge" > 03 - 04 - 2015 </span>    
-                </center>
-          </div>
-          </div>
-  </a>
-    <br>
-
-    
-     <a href="#" class="list-group-item">
-   <div class="row">
-          <div class="col-md-8">
-    <h4 class="list-group-item-heading">University Grants Commission</h4>
-    <p class="list-group-item-text"><b>Posted By:</b> Dr. Vishal Sharma</p>
-    </div>
-          <div class="col-md-4" style="padding-top: 10px;">
-                <center>
-                    <span class="badge" > 19 - 02 - 2015 </span>    
-                </center>
-          </div>
-          </div>
-  </a>
-    <br>
-
-     <a href="#" class="list-group-item">
-    <div class="row">
-          <div class="col-md-8">
-    <h4 class="list-group-item-heading">Student scholarship notice and form</h4>
-    <p class="list-group-item-text"><b>Posted By:</b> Dr. Vishal Sharma</p>
-    </div>
-          <div class="col-md-4" style="padding-top: 10px;">
-                <center>
-                    <span class="badge" > 25 - 11 - 2014 </span>    
-                </center>
-          </div>
-          </div>
-  </a>
-    <br>
-    
-
- <a href="#" class="list-group-item">
-    <div class="row">
-          <div class="col-md-8">
-    <h4 class="list-group-item-heading">Legal Aid Clinic</h4>
-    <p class="list-group-item-text"><b>Posted By:</b> Dr. Vishal Sharma</p>
-    </div>
-          <div class="col-md-4" style="padding-top: 10px;">
-                <center>
-                    <span class="badge" > 09 - 09 - 2014 </span>    
-                </center>
-          </div>
-          </div>
-  </a>
-    <br>
-    
- <a href="#" class="list-group-item">
-    <div class="row">
-          <div class="col-md-8">
-    <h4 class="list-group-item-heading">Information regarding travel allowance</h4>
-    <p class="list-group-item-text"><b>Posted By:</b> Dr. Vishal Sharma</p>
-    </div>
-          <div class="col-md-4" style="padding-top: 10px;">
-                <center>
-                    <span class="badge" > 13 - 08 - 2014 </span>    
-                </center>
-          </div>
-          </div>
-  </a>
-    <br>
-    
-    
- <a href="#" class="list-group-item">
-    <div class="row">
-          <div class="col-md-8">
-    <h4 class="list-group-item-heading">Phd in Buddhist Studies and Civilization</h4>
-    <p class="list-group-item-text"><b>Posted By:</b> Dr. Vishal Sharma</p>
-    </div>
-          <div class="col-md-4" style="padding-top: 10px;">
-                <center>
-                    <span class="badge" > 21 - 06 - 2014 </span>    
-                </center>
-          </div>
-          </div>
-  </a>
-    <br>
-    
- <a href="#" class="list-group-item">
-    <div class="row">
-          <div class="col-md-8">
-    <h4 class="list-group-item-heading">Change in Grading Scheme</h4>
-    <p class="list-group-item-text"><b>Posted By:</b> Dr. Vishal Sharma</p>
-    </div>
-          <div class="col-md-4" style="padding-top: 10px;">
-                <center>
-                    <span class="badge" > 15 - 02 - 2014 </span>    
-                </center>
-          </div>
-          </div>
-  </a>
-    <br>
-    
-    
-    
-     </div>
-
-
-
-
-
-
-
-
-</div> 
+			</div>
+		</div> 
