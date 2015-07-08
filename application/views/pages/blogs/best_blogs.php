218b7160b9580b2304283be6f3126c28bb02c6c9
Rajat Saxena <rajat.saxena035@gmail.com>
2015-07-09 03:35:28 +0530

    Blogs data in loops - as recommended by Bhawesh

--- application/views/pages/blogs/best_blogs.php
+++ application/views/pages/blogs/best_blogs.php
@@ -1,191 +1,62 @@
 
-
+<!--This page is sorted according to the "Stars(Likes)" of a blog-->
    
+             
  
 <div class="container-fluid" style=" margin-right: 10px; margin-left: 10px;" >
-     <div class="row">
+   <div class="row">
+		<div class="col-md-9" style="padding-right:30px; border-right: 2px solid #ccc;">        
+			<div class="well well-sm"> 
+				<ul class="nav nav-pills nav-justified">
+					<li role="presentation" ><a href="<?php echo site_url('blogs/write_blogs')?>">Write Blogs</a></li>
+					<li role="presentation" ><a href="<?php echo site_url('blogs/recent_blogs')?>">Recent Blogs</a></li>
+					<li role="presentation" class="active"><a href="<?php echo site_url('blogs/best_blogs')?>">Best Blogs</a></li>
+				</ul>
+			</div>        
+			<br>
     
-	<div class="col-md-9" style="padding-right:30px; border-right: 2px solid #ccc;">        
- 
-            
-   
-            
-           <div class="well well-sm"> 
-            
-            <ul class="nav nav-pills nav-justified">
-  <li role="presentation" ><a href="<?php echo site_url('blogs/write_blogs')?>">Write Blogs</a></li>
-                
-  <li role="presentation" ><a href="<?php echo site_url('blogs/recent_blogs')?>">Recent Blogs</a></li>
-  
-  <li role="presentation" class="active"><a href="<?php echo site_url('blogs/best_blogs')?>">Best Blogs</a></li>
-  
-</ul>
-            
-         </div>   
-            
-    <br>
-    
-    <div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title"><b>Artificial Intelligence - The next step towards human destruction</b></h3>
-  </div>
-  <div class="panel-body">
-    <p align="justify">
-    Artificial Intelligence is one of the hot topics of the current decade. All 
-    the major research nowadays is on AI. The computer scientists all over the world are
-    inventing new algorithms that would help robots take its own decisions and learn from its past.
-    But a little is paid towards the destruction it would cause once its actually implemented.
-    What if the Robots defeat... <a href="#">Read More</a><br>
-    </p>    
-  </div>
-   <div class="panel-footer">
-   
-	<div class="row">
-	
-	<div class="col-md-4">
-    
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> &nbsp;<span class="badge">15</span></button>&nbsp;
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span> &nbsp;<span class="badge">4</span></button>&nbsp;
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> &nbsp;<span class="badge">9</span></button>
-	</div>
-    
-	
-	
-   
-   <div class="col-md-8" style="text-align: right;  padding-top: 5px;">
-   <b>Aman Verma - 14/ICS/005</b>
-   </div>
-   
-  
-   </div>
-   </div>
-   
-   </div>
+			<!--Loop Starts-->
+			
+			<?php
+				for ($x = 0; $x <= 3; $x++)
+				{
+			?>
+		
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title"><b>Artificial Intelligence - The next step towards human destruction</b></h3>
+				</div>
+				<div class="panel-body">
+					<p align="justify">
+						Artificial Intelligence is one of the hot topics of the current decade. All 
+						the major research nowadays is on AI. The computer scientists all over the world are
+						inventing new algorithms that would help robots take its own decisions and learn from its past.
+						But a little is paid towards the destruction it would cause once its actually implemented.
+						What if the Robots defeat iti only first three lines of a blog to come here... <a href="#">Read More</a><br>
+					</p>    
+				</div>
+				<div class="panel-footer">
+					<div class="row">
+						<div class="col-md-4">
+							<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> &nbsp;<span class="badge">15</span></button>&nbsp;
+							<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span> &nbsp;<span class="badge">4</span></button>&nbsp;
+							<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> &nbsp;<span class="badge">9</span></button>
+						</div>
+						<div class="col-md-8" style="text-align: right;  padding-top: 5px;">
+							<b>Aman Verma - 14/ICS/005</b>
+						</div>
+					</div>
+				</div>
+			</div>
 
+			<?php	
+				} 
+			?>
+			
+			<!--Loop Ends-->
    
 
-  
-    
-    <div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title"><b>Artificial Intelligence - The next step towards human destruction</b></h3>
-  </div>
-  <div class="panel-body">
-    <p align="justify">
-    Artificial Intelligence is one of the hot topics of the current decade. All 
-    the major research nowadays is on AI. The computer scientists all over the world are
-    inventing new algorithms that would help robots take its own decisions and learn from its past.
-    But a little is paid towards the destruction it would cause once its actually implemented.
-    What if the Robots defeat... <a href="#">Read More</a><br>
-    </p>    
-  </div>
-   <div class="panel-footer">
-   
-	<div class="row">
-	
-	<div class="col-md-4">
-    
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> &nbsp;<span class="badge">15</span></button>&nbsp;
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span> &nbsp;<span class="badge">4</span></button>&nbsp;
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> &nbsp;<span class="badge">9</span></button>
-	</div>
-    
-	
-	
-   
-   <div class="col-md-8" style="text-align: right;  padding-top: 5px;">
-   <b>Aman Verma - 14/ICS/005</b>
-   </div>
-   
-  
-   </div>
-   </div>
-   
-   </div>
-
-   
-
-  
-    
-    
-  <div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title"><b>Artificial Intelligence - The next step towards human destruction</b></h3>
-  </div>
-  <div class="panel-body">
-    <p align="justify">
-    Artificial Intelligence is one of the hot topics of the current decade. All 
-    the major research nowadays is on AI. The computer scientists all over the world are
-    inventing new algorithms that would help robots take its own decisions and learn from its past.
-    But a little is paid towards the destruction it would cause once its actually implemented.
-    What if the Robots defeat... <a href="#">Read More</a><br>
-    </p>    
-  </div>
-   <div class="panel-footer">
-   
-	<div class="row">
-	
-	<div class="col-md-4">
-    
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> &nbsp;<span class="badge">15</span></button>&nbsp;
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span> &nbsp;<span class="badge">4</span></button>&nbsp;
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> &nbsp;<span class="badge">9</span></button>
-	</div>
-    
-	
-	
-   
-   <div class="col-md-8" style="text-align: right;  padding-top: 5px;">
-   <b>Aman Verma - 14/ICS/005</b>
-   </div>
-   
-  
-   </div>
-   </div>
-   
-   </div>
-
-   
-
-    
-  <div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title"><b>Artificial Intelligence - The next step towards human destruction</b></h3>
-  </div>
-  <div class="panel-body">
-    <p align="justify">
-    Artificial Intelligence is one of the hot topics of the current decade. All 
-    the major research nowadays is on AI. The computer scientists all over the world are
-    inventing new algorithms that would help robots take its own decisions and learn from its past.
-    But a little is paid towards the destruction it would cause once its actually implemented.
-    What if the Robots defeat... <a href="#">Read More</a><br>
-    </p>    
-  </div>
-   <div class="panel-footer">
-   
-	<div class="row">
-	
-	<div class="col-md-4">
-    
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> &nbsp;<span class="badge">15</span></button>&nbsp;
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-share-alt" aria-hidden="true"></span> &nbsp;<span class="badge">4</span></button>&nbsp;
-	<button type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target=".blogs"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> &nbsp;<span class="badge">9</span></button>
-	</div>
-    
-	
-	
-   
-   <div class="col-md-8" style="text-align: right;  padding-top: 5px;">
-   <b>Aman Verma - 14/ICS/005</b>
-   </div>
-   
-  
-   </div>
-   </div>
-   
-   </div>
-
-        </div>
+		</div>
 
 
  <!--Modal-->
