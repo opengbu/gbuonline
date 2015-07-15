62751949dd583ae883d67d2d5116769557940f67
Sarthak Garg <gargsarthak30@gmail.com>
2015-07-15 12:01:08 +0530

    Row 4 updated

--- application/views/pages/row4.php
+++ application/views/pages/row4.php
@@ -1,209 +1,36 @@
  
 <div class="row">
     <div class = "container">
-     <div class="col-md-4">
-            <hr>
-
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Main Nav.</h3>
-                </div>
-                
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="headingOne">
-                    <h4 class="panel-title">
-                        <a class="collapsed" onclick="window.scroll(, )" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                            Clubs
-                        </a>
-                    </h4>
-                </div>
-                <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-                    <div class="panel-body">
-
-                        <a href="<?php echo site_url('clubs/robotics') ?>">1. ROBOTICS CLUB</a><br><br>
-                        <a href="<?php echo site_url('clubs/programming') ?>">2. PROGRAMMING CLUB</a><br><br>
-                        <a href="<?php echo site_url('clubs/debating') ?>">3. DEBATING CLUB</a><br><br>
-                        <a href="<?php echo site_url('clubs/art') ?>">4. ART CLUB</a><br><br>
-                        <a href="<?php echo site_url('clubs/ss') ?>">5. SOCIAL SERVICE CLUB</a><br><br>
-                        <a href="<?php echo site_url('clubs/dramatics') ?>">6. DRAMATICS CLUB</a><br><br>
-                        <a href="<?php echo site_url('clubs/adventure') ?>">7. ADVENTURE CLUB</a><br><br>
-                        <a href="<?php echo site_url('clubs/literary') ?>">8. LITERARY CLUB</a><br><br>
-                        <a href="<?php echo site_url('clubs/photography') ?>">9. PHOTOGRAPHY CLUB</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="headingTwo">
-                    <h4 class="panel-title">
-                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Downloads
-                        </a>
-                    </h4>
-                </div>
-                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-                    <div class="panel-body">
-                        <ul>
-
-                            <b><center>Mobile Apps</center></b><br>
-
-                            <li><a href="#">Time Table (Android)</a></li>
-                            <li><a href="#">Time Table (Windows)</a></li>
-                            <li><a href="#">Abhivyanjana</a></li>
-                            <li><a href="#">Attendance</a></li>
-                            <br>
-
-                            <b><center>Utility Softwares</center></b><br>
-
-                            <li><a href="#">Proxifier</a></li>
-                            <li><a href="#">IDM</a></li>
-                            <li><a href="#">C-Cleaner</a></li>
-                            <li><a href="#">Ultra-surf</a></li>
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="panel panel-default">
-
-                <div class="panel-heading" role="tab" id="headingThree">
-                    <h4 class="panel-title">
-                        <a class="collapsed" onclick="window.scroll(, )" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Student Chapters
-                        </a>
-                    </h4>
-                </div>
-                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-                    <div class="panel-body">
-                        The world's largest professional association for the advancement of technology.<br>
-                        <a href='http://www.ieeegbu.weebly.com'>IEEE-GBU Student Branch</a><hr>
-                        Codechef is a global programming community.To promote competitive programming we have a 
-                        Codechef-GBU Chapter.<br>
-                        <a href="http://www.codechef.com/campus_chapter/CodeChef-GBU">Codechef-GBU Chapter</a><hr>
-                        HackerEarth is a coding platform allowing students to participate in programming challenges.
-                        GBU conducts its own challenges under the series "Code-In-GBU".<br>
-                        <a href="https://www.hackerearth.com/code-in-gbu/">Code-In-GBU</a>
-
-                    </div>
-                </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="headingFour">
-                    <h4 class="panel-title">
-                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                            Major Events
-                        </a>
-                    </h4>
-                </div>
-                <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-                    <div class="panel-body">
-
-                        <a href="#">ABHIVYANJANA</a><hr>
-                        <a href="#">SHOURYOTSAV</a><hr>
-                        <a href="#">ECOKART</a>   
-                    </div>
-                </div>
-            </div>
-
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="headingFive">
-                    <h4 class="panel-title">
-                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                            Placements
-                        </a>
-                    </h4>
-                </div>
-                <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
-                    <div class="panel-body">
-
-                        <ul>
-                            <li><a href="<?php echo base_url('resources/placements/p2015.pdf') ?>">Placement 2015</a></li><hr>
-                            <li><a href="<?php echo base_url('resources/placements/p2014.pdf') ?>">Placement 2014</a></li><hr>
-                            <li><a href="<?php echo base_url('resources/placements/p2013.pdf') ?>">Placement 2013</a></li><hr>
-                            <li><a href="<?php echo base_url('resources/placements/p2012.pdf') ?>">Placement 2012</a></li><hr>
-                            <li><a href="<?php echo base_url('resources/placements/p2011.pdf') ?>">Placement 2011</a></li><hr>
-                            <li><a href="<?php echo base_url('resources/placements/p2010.pdf') ?>">Placement 2010</a></li>
-                        </ul>          
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="headingSix">
-                    <h4 class="panel-title">
-                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-                            List Of Holidays
-                        </a>
-                    </h4>
-                </div>
-                <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
-                    <div class="panel-body">
-
-                        <a href="#">View</a>
-
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="headingSeven">
-                    <h4 class="panel-title">
-                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
-                            E-Books
-                        </a>
-                    </h4>
-                </div>
-                <div id="collapseSeven" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSeven">
-                    <div class="panel-body">
-                        A collection of books available online for free.<br>
-                        <a href="<?php echo site_url('ebooks/ebooks_home') ?>">Enter here</a>
-
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="headingEight">
-                    <h4 class="panel-title">
-                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
-                            Official GBU Website
-                        </a>
-                    </h4>
-                </div>
-                <div id="collapseEight" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingEight">
-                    <div class="panel-body">
-
-                        <a href="http://www.gbu.ac.in">Enter here</a>
-
-
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
+     <div class="col-md-9" >
+            <h2 align="center"><font face="Times New Roman"><b>News and Updates</b></font></h2>
+<hr>
+            <div class="list-group ">
+  <a href="<?php echo site_url('feat/events') ?>" class="list-group-item">
+    <h4 class="list-group-item-heading"><b>Code-In-GBU</b></h4>
+    <p class="list-group-item-text">An online coding competition being organised by the ICT department next Wednesday.So get ready to show your coding skills.Exciting prizes to be won.</p>
+  </a>
+</div>
+<div class="list-group">
+  <a href="<?php echo site_url('feat/vnb') ?>" class="list-group-item ">
+    <h4 class="list-group-item-heading"><b>Change in Grading System</b></h4>
+    <p class="list-group-item-text">Grading System for students of session 2014-2015 and onwards has been changed.</p>
+  </a>
+</div>
+<div class="list-group">
+  <a href="#" class="list-group-item">
+    <h4 class="list-group-item-heading"><b>Imagine Cup</b></h4>
+    <p class="list-group-item-text">Microsoft's Imagine Cup is the world's most prestigious student technology competition, bringing together student innovators from all over the world. If you have a ...</p>
+  </a>
+</div>
+<div class="list-group">
+  <a href="#" class="list-group-item">
+    <h4 class="list-group-item-heading"><b>Notification for Odd semester 2015-2016 session</b></h4>
+    <p class="list-group-item-text">ede rf wf wf wfd   qdwqdqdasd rfr fw  cwefq dd qdwrrfwfcdax wdqdqddasef wefwef ewfew.</p>
+  </a>
+</div>
      </div>
 
-        <div class="col-md-5">
-            <hr>
-
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Recent News</h3>
-                </div>
-                <div class="panel-body">
-                    <?php foreach ($news as $news_item): ?> 
-                        <?php echo "<li>" . $news_item['title'] . "</li>" ?>
-                        <p><a href="news/<?php echo $news_item['slug'] ?>">View article</a></p>
-
-                    <?php endforeach ?>
-                </div>
-            </div>
-        </div>
-
+        
         <div class="col-md-3">
             <h2 align="center"><font face="Times New Roman"><b>Extras</b></font></h2>
             <hr>
@@ -333,15 +160,16 @@
                     <div class="panel-heading" role="tab" id="headingSix">
                         <h4 class="panel-title">
                             <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-                                List Of Holidays
+                                University Calendar
                             </a>
                         </h4>
                     </div>
                     <div id="collapseSix" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingSix">
                         <div class="panel-body">
 
-                            <a href="#">View</a>
-
+                            <a href="#">Academic Calendar</a><br>
+							<a href="#">List Of Holidays</a>
+								
 
                         </div>
                     </div>
--- application/views/pages/slider.php
+++ application/views/pages/slider.php
@@ -1,21 +1,9 @@
 <div class="row"><!--row3 for slider-->
-     
+
+      
 <div class="slider">
-     <img src="<?php echo base_url('resources/images/image1.jpg')?>" height="500"/>
-     <img src="<?php echo base_url('resources/images/image2.jpg')?>" height="500"/>
-     <img src="<?php echo base_url('resources/images/image3.jpg')?>" height="500"/>
-     <img src="<?php echo base_url('resources/images/image4.jpg')?>" height="500"/>
-    
-     <!--<img src="<?php echo base_url('resources/images/image6.jpg')?>" height="500"/>-->
-    <!-- <img src="<?php echo base_url('resources/images/image7.jpg')?>" height="500"/>
-     <img src="<?php echo base_url('resources/images/image8.jpg')?>" height="500"/>-->          
-     
-     <!--<img src="<?php echo base_url('resources/images/image10.jpg')?>" height="500"/>-->
-     <img src="<?php echo base_url('resources/images/image11.jpg')?>" height="500"/>
-     <!--<img src="<?php echo base_url('resources/images/image12.jpg')?>" height="500"/>-->
-     <!--<img src="<?php echo base_url('resources/images/image13.jpg')?>" height="500"/>
-     <img src="<?php echo base_url('resources/images/image14.jpg')?>" height="500"/>-->                                  
- 
- </div>
+     <img src="<?php echo base_url('resources/images/collage.jpeg')?>" height="450"/>
+</div>
+
 </div><!--row3 for slider-->
  
