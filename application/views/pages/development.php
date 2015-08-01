b460c219dd4108b63e8b1106c6a532567d795b25
rishabh279 <rishabhahuja279@gmail.com>
2015-08-02 02:51:38 +0530

    added releases page

---
+++ application/views/pages/development.php
@@ -0,0 +1,23 @@
+<div class="container-fluid" style=" margin-right: 10px; margin-left: 10px;" >    
+
+
+    <div class="row">
+
+        <div class="col-md-9" style="padding-right:30px; border-right: 2px solid #ccc;">        
+		<ul class="list-group">
+  <li class="list-group-item">
+    <span class="badge"></span>
+    Version 1.0
+  </li>
+  <li class="list-group-item">
+    <span class="badge"></span>
+    Version 2.0
+  </li>
+  <li class="list-group-item">
+    <span class="badge"></span>
+    Version 3.0
+  </li>
+</ul>
+           
+
+        </div> 
---
+++ application/views/pages/developmnt.php
@@ -0,0 +1,10 @@
+<div class="container-fluid" style=" margin-right: 10px; margin-left: 10px;" >    
+
+
+    <div class="row">
+
+        <div class="col-md-9" style="padding-right:30px; border-right: 2px solid #ccc;">        
+
+            
+
+        </div> 
