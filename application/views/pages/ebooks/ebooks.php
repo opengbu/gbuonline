3562697b1685adaf118bda308e8e8c5541ee0196
bhawesh chandola <bhawesh.chandola@gmail.com>
2015-08-01 12:53:26 +0530

    no of books available displayed through count.

--- application/views/pages/ebooks/ebooks.php
+++ application/views/pages/ebooks/ebooks.php
@@ -11,36 +11,108 @@
 		
         <ul class="list-group">
                 <li class="list-group-item">
-                    <span class="badge">16
+                    <span class="badge"><?php
+                    $q=$this->db->query("select count(*) as row_count from ebooks where sc_name='soict'");
+                    $results=$q->result();
+                    foreach($results as $rows)
+                    {
+                        
+                        echo $rows->row_count;    
+                        
+                    }
+                    ?>
                     </span>
                         <a href="<?php echo site_url('ebooks/soict_books')?>">SOICT </a>  
                 </li>
                 <li class="list-group-item">
-                        <span class="badge">16</span>
+                        <span class="badge"><?php
+                    $q=$this->db->query("select count(*) as row_count from ebooks where sc_name='som'");
+                    $results=$q->result();
+                    foreach($results as $rows)
+                    {
+                        
+                        echo $rows->row_count;    
+                        
+                    }
+                    ?></span>
                         <a href="<?php echo site_url('ebooks/som_books')?>">SOM </a>  
                 </li>
                 <li class="list-group-item">
-                        <span class="badge">16</span>
+                        <span class="badge"><?php
+                    $q=$this->db->query("select count(*) as row_count from ebooks where sc_name='sobt'");
+                    $results=$q->result();
+                    foreach($results as $rows)
+                    {
+                        
+                        echo $rows->row_count;    
+                        
+                    }
+                    ?></span>
                         <a href="<?php echo site_url('ebooks/sobt_books')?>">SOBT </a>  
                 </li>
                 <li class="list-group-item">
-                        <span class="badge">16</span>
+                        <span class="badge"><?php
+                    $q=$this->db->query("select count(*) as row_count from ebooks where sc_name='sovsas'");
+                    $results=$q->result();
+                    foreach($results as $rows)
+                    {
+                        
+                        echo $rows->row_count;    
+                        
+                    }
+                    ?></span>
                         <a href="<?php echo site_url('ebooks/sovsas_books')?>"> SOVSAS</a>
                 </li>
                 <li class="list-group-item">
-                        <span class="badge">16</span>
+                        <span class="badge"><?php
+                    $q=$this->db->query("select count(*) as row_count from ebooks where sc_name='soe'");
+                    $results=$q->result();
+                    foreach($results as $rows)
+                    {
+                        
+                        echo $rows->row_count;    
+                        
+                    }
+                    ?></span>
                         <a href="<?php echo site_url('ebooks/soe_books')?>"> SOE </a>  
                 </li>
                 <li class="list-group-item">
-                        <span class="badge">16</span>
+                        <span class="badge"><?php
+                    $q=$this->db->query("select count(*) as row_count from ebooks where sc_name='sobsc'");
+                    $results=$q->result();
+                    foreach($results as $rows)
+                    {
+                        
+                        echo $rows->row_count;    
+                        
+                    }
+                    ?></span>
                         <a href="<?php echo site_url('ebooks/sobsc_books')?>"> SOBSC </a>  
                 </li>
                 <li class="list-group-item">
-                        <span class="badge">16</span>
+                        <span class="badge"><?php
+                    $q=$this->db->query("select count(*) as row_count from ebooks where sc_name='sohss'");
+                    $results=$q->result();
+                    foreach($results as $rows)
+                    {
+                        
+                        echo $rows->row_count;    
+                        
+                    }
+                    ?></span>
                         <a href="<?php echo site_url('ebooks/sohss_books')?>">SOHSS </a>  
                 </li>
                 <li class="list-group-item">
-                        <span class="badge">16</span>
+                        <span class="badge"><?php
+                    $q=$this->db->query("select count(*) as row_count from ebooks where sc_name='soljg'");
+                    $results=$q->result();
+                    foreach($results as $rows)
+                    {
+                        
+                        echo $rows->row_count;    
+                        
+                    }
+                    ?></span>
                         <a href="<?php echo site_url('ebooks/soljg_books')?>">SOLJG </a>  
                 </li>
 </ul>
