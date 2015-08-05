64b34469de88e6a5853052d3570275d51bdb3068
Rajat Saxena <rajat.saxena035@gmail.com>
2015-08-05 15:30:57 +0530

    Changed red color
    Page Heading color changed to a gradient "Hersheys".

--- application/views/pages/common/page-heading.php
+++ application/views/pages/common/page-heading.php
@@ -1,6 +1,21 @@
+<style>
+#pagehead {
+  background: -webkit-linear-gradient(90deg, #1e130c 10%, #9a8478 90%); /* Chrome 10+, Saf5.1+ */
+  background:    -moz-linear-gradient(90deg, #1e130c 10%, #9a8478 90%); /* FF3.6+ */
+  background:     -ms-linear-gradient(90deg, #1e130c 10%, #9a8478 90%); /* IE10 */
+  background:      -o-linear-gradient(90deg, #1e130c 10%, #9a8478 90%); /* Opera 11.10+ */
+  background:         linear-gradient(90deg, #1e130c 10%, #9a8478 90%); /* W3C */
+  padding-top: 10px; 
+  padding-left: 50px; 
+  margin-top: 15px;
+  }
+</style>
+
+
+
 <div class="row">
 
-<div class="page-header" style = "background-color: darkred; padding-top: 10px; padding-left: 50px; margin-top: 15px;">
+<div class="page-header" id="pagehead">
             <font color="white" face="Monotype Corsiva">
             <h1><?php echo $heading; ?></br>
                 <small><?php echo $message; ?></small>
