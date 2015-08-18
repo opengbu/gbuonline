bd738f0d7167525e7f98d61707c5a8e8814b8920
varun-garg <varun.10@live.com>
2015-08-18 16:35:49 +0000

    (Front End) Blog is unliked if like button is clicked again

--- application/controllers/Blogs.php
+++ application/controllers/Blogs.php
@@ -62,22 +62,23 @@ class Blogs extends CI_Controller {
     }
 
     public function like() {
-        
+
         if ($this->session->userdata('loggedin') != 1) //student/user/admin logged in
             redirect('users?redirect=' . current_url());
-        
+
         $blog_id = $this->input->get("blog_id");
         $user_id = $this->session->userdata("user_id");
-        
-        $num = $this->db->query("select * from blog_likes where blog_id = '$blog_id' and user_id = '$user_id'");
-        if($num->num_rows() == 0)
-        $this->db->query("insert into blog_likes (blog_id,user_id) VALUES ('$blog_id', '$user_id')");
 
+        $num = $this->db->query("select * from blog_likes where blog_id = '$blog_id' and user_id = '$user_id'");
+        if ($num->num_rows() == 0)
+            $this->db->query("insert into blog_likes (blog_id,user_id) VALUES ('$blog_id', '$user_id')");
+        else
+            $this->db->query("delete from blog_likes where blog_id = '$blog_id' and user_id = '$user_id'");
+             
         if (isset($_REQUEST['redirect2']) && $_REQUEST['redirect2'] != "") //anyone wants to get back            
             redirect($_REQUEST['redirect2']);
-            
-        else redirect('blogs/recent_blogs');
-        
-        }
+        else
+            redirect('blogs/recent_blogs');
     }
-    
+
+}
