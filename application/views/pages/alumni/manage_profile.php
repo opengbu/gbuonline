bb674052b47c638dcf402e2000e018801d38c76d
Rajat Saxena <rajat.saxena035@gmail.com>
2015-09-01 22:11:05 +0530

    created pages

---
+++ application/views/pages/alumni/manage_profile.php
@@ -0,0 +1,20 @@
+<div class="container-fluid" style=" margin-right: 10px; margin-left: 10px;" >
+    <div class="row">
+	
+	<div class="well well-sm">
+		<ul class="nav nav-pills nav-justified">
+  <li role="presentation"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Distinguished Alumni</a></li>
+  <li role="presentation"><a href="#">Alumni List</a></li>
+  <li role="presentation"><a href="#">Events</a></li>
+  <li role="presentation"><a href="#">Gallery</a></li>
+  <li role="presentation" class="active"><a href="#">Manage Profile</a></li>
+		</ul>
+	</div>
+		<br> 
+		
+		<!--your content-->
+		
+		<br>
+	</div>
+</div>
