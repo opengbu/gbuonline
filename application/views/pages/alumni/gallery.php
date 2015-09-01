d67a6faf9d80debf08f1fbb076884dd03a045002
Sarthak Garg <gargsarthak30@gmail.com>
2015-09-02 00:04:37 +0530

    gallery page uploaded (in and out )

---
+++ application/views/pages/alumni/gallery.php
@@ -0,0 +1,85 @@
+<div class="container-fluid" style=" margin-right: 10px; margin-left: 10px;" >
+    <div class="row">
+	
+	<div class="well well-sm">
+		<ul class="nav nav-pills nav-justified">
+  <li role="presentation"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Distinguished Alumni</a></li>
+  <li role="presentation"><a href="#">Alumni List</a></li>
+  <li role="presentation" ><a href="#">Events</a></li>
+  <li role="presentation" class="active"><a href="#" >Gallery</a></li>
+  <li role="presentation"><a href="#">Manage Profile</a></li>
+		</ul>
+	</div>
+		<br> 
+		<div class="row">
+	
+	
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<br>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+  <br>
+  <div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+<div class="col-xs-6 col-md-3">
+    <a href="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" class="thumbnail">
+      <img src="<?php echo base_url('resources/alumni_gallery/willsmith.jpeg')?>" alt="">
+    </a>
+  </div>
+</div>
+		
+		
+		<br>
+	</div>
+</div>
