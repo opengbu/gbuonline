70a917d3dd7a96ecdd68ef2f352a00dd8a222ec0
KemeBrown <shobhit281295@gmail.com>
2015-09-01 22:40:11 +0530

    AlumniList and Sort By

--- application/controllers/Alumni.php
+++ application/controllers/Alumni.php
@@ -47,15 +47,45 @@ class Alumni extends CI_Controller {
         $this->load->view('pages/alumni/alumniList');
         $this->load->view('pages/common/footer');
     }
+    public function name() {
+        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
+        $data['heading'] = ' Alumni List ';
+        $data['message'] = 'Let the world know you.....';
+        $this->load->view('pages/common/link', $data);
+        $this->load->view('pages/common/header');
+        $this->load->view('pages/common/page-heading', $data);
+        $this->load->view('pages/alumni/alumniList');
+        $this->load->view('pages/common/footer');
+    }
+    public function cname() {
+        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
+        $data['heading'] = ' Alumni List ';
+        $data['message'] = 'Let the world know you.....';
+        $this->load->view('pages/common/link', $data);
+        $this->load->view('pages/common/header');
+        $this->load->view('pages/common/page-heading', $data);
+        $this->load->view('pages/alumni/cname');
+        $this->load->view('pages/common/footer');
+    }
+    public function company() {
+        $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
+        $data['heading'] = ' Alumni List ';
+        $data['message'] = 'Let the world know you.....';
+        $this->load->view('pages/common/link', $data);
+        $this->load->view('pages/common/header');
+        $this->load->view('pages/common/page-heading', $data);
+        $this->load->view('pages/alumni/company');
+        $this->load->view('pages/common/footer');
+    }
 
-    public function distinguished() {
+    public function location() {
         $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
         $data['heading'] = ' GBU Alumni ';
         $data['message'] = 'Let the world know you.....';
         $this->load->view('pages/common/link', $data);
         $this->load->view('pages/common/header');
         $this->load->view('pages/common/page-heading', $data);
-        $this->load->view('pages/alumni/distinguished');
+        $this->load->view('pages/alumni/location');
         $this->load->view('pages/common/footer');
     }
 
--- application/views/pages/alumni/alumniList.php
+++ application/views/pages/alumni/alumniList.php
@@ -1,28 +1,63 @@
 <div class="container-fluid" style=" margin-right: 10px; margin-left: 10px;" >  
-    <div class="row">
-        
-<!--     <div class="dropdown" style="margin-left:970px;size:25px">
+<div class="row">
+    <div class="dropdown" style="margin-left:970px;size:25px">
 					
-					<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" style="size:25px;">
+					<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" style="size:25px;margin-top:-80px;">
 						Sort By :
 						<span class="caret"></span>
 					</button>
 					
-					<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-						<li><a href="<?php echo site_url('hostels/hname')?>">Hostel Name</a></li>
-						<li><a href="<?php echo site_url('hostels/wname')?>">Warden Name</a></li>
-						<li><a href="<?php echo site_url('hostels/woffice')?>">Warden Office</a></li>
+					<ul class="dropdown-menu" aria-labelledby="dropdownMenu1" style="margin-top:-60px;">
+						<li><a href="<?php echo site_url('alumni/name')?>">Name</a></li>
+						<li><a href="<?php echo site_url('alumni/cname')?>">Course Name</a></li>
+                        <li><a href="<?php echo site_url('alumni/company')?>">Company</a></li>
+						<li><a href="<?php echo site_url('alumni/location')?>">Location</a></li>
 					</ul>
 				    
-        </div> -->  
-    </div>
+				</div>   
+    </div><br/>
     <div class="jumbotron" >
-           <!-- <h2><font face="Impact">List Of Respective Wardens</font></h2><br/>-->
         <div class="row"><font face="Georgia Bold">
-            <div class="col-md-4"><b>Name</b></div>
-            <div class="col-md-4"><b>Alloted Course</b></div>
-            <div class="col-md-4"><b>Currently Working in</b></div>
+            <div class="col-md-3"><b>Name</b></div>
+            <div class="col-md-3"><b>Alloted Course</b></div>
+            <div class="col-md-3"><b>Currently Working at</b></div>
+            <div class="col-md-3"><b>Current Location</b></div>
        </font></div><hr /><hr />
+        
+        <?php
+            $r=$this->db->query("select users.username,edu_info.course_name,work_details.location,work_details.company_name from users,edu_info,alumni_basic,work_details");
+            $results=$r->result();
+            
+            foreach ($results as $rows)
+            {
+        ?>
+            <div class="row">    
+            <div class="col-md-2">
+            <?php
+                echo $row->username;
+            ?>
+            </div>
+            <div class="col-md-2">
+            <?php
+                echo $row->course_name;
+            ?>
+            </div>
+            <div class="col-md-2">
+            <?php
+                echo $row->company_name;
+            ?>
+            </div>
+            <div class="col-md-2">
+            <?php
+                echo $row->location;
+            ?>
+            </div>
+            </div><hr/>
+        <?php
+            }
+        ?>
+            </div>
+                   
         
     </div>
 </div>
