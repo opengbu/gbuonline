95627a5e7b6372be4437bd0715438f01367e7766
Rajat Saxena <rajat.saxena035@gmail.com>
2015-09-02 05:44:39 +0530

    Alumni Homepage

--- application/views/pages/alumni/home.php
+++ application/views/pages/alumni/home.php
@@ -11,10 +11,240 @@
   <li role="presentation"><a href="<?= site_url('alumni');?>">Manage Profile</a></li>
 		</ul>
 	</div>
-		<br> 
 		
+		<br/>
 		<!--your content-->
+	<div>
+		<center>
+		<h2><span class="label label-danger">Alumni Community</span></h2>
+		<br><br>
+		<p>Gautam Buddha University alumni community has more than 3000 members.<br>
+		   The university aims to produce future leaders and entrepreneurs who can inspire the next generation.<br/>
+		   The objective of this Alumni Community is to provide a platform to connect and discover GBUites present across the globe. 
+		</p>
+		<br/><hr/><br/>
+		</center>
+	</div>
+	
+	
+	<div>
+		<h2 align="center"><span class="label label-danger">Recent Appointments</span></h2>
+		<br><br>
+			<div class="container">
+				<div class="row">
+				<div class="col-md-1">
+				</div>
+				<div class="col-md-5">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-md-5">
+									<img id="tn" src="<?php echo base_url('resources/images/rajat.jpg')?>" height="170" width="130"/>
+								</div>
+								<div class="col-md-7" id="spn">
+									Name : Rajat Saxena <br/>
+									Passout : 2018<br/>
+									School : SoICT<br/><br/>
+									Status : Joined Microsoft <br/>
+									Position : Data Scientist <br/>
+									Location : San Francisco
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-5">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-md-5">
+									<img id="tn" src="<?php echo base_url('resources/images/shobhit.jpg')?>" height="170" width="130"/>
+								</div>
+								<div class="col-md-7" id="spn">
+									Name : Shobhit Sharma <br/>
+									Passout : 2016<br/>
+									School : SoHSS<br/><br/>
+									Status : Joined Google <br/>
+									Position : S/W Engineer <br/>
+									Location : New York
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-1">
+				</div>
+				</div>	
+				
+				<div class="row">
+				<div class="col-md-1">
+				</div>
+				<div class="col-md-5">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-md-5">
+									<img id="tn" src="<?php echo base_url('resources/images/varun.jpg')?>" height="170" width="130"/>
+								</div>
+								<div class="col-md-7" id="spn">
+									Name : Varun Garg <br/>
+									Passout : 2020<br/>
+									School : SoVSAS<br/><br/>
+									Status : Joined Facebook <br/>
+									Position : Backend Engineer <br/>
+									Location : California
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-5">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="row">
+								<div class="col-md-5">
+									<img id="tn" src="<?php echo base_url('resources/images/bhawesh.jpg')?>" height="170" width="130"/>
+								</div>
+								<div class="col-md-7" id="spn">
+									Name : Bhawesh Chandola <br/>
+									Passout : 2029<br/>
+									School : SoICT<br/><br/>
+									Status : Joined Oracle <br/>
+									Position : Data Scientist <br/>
+									Location : Hyderabad
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-1">
+				</div>
+				</div>		
+				
+			</div><br/>
+			<hr/>
+		</div>
 		
-		<br>
+		
+		
+		<div>
+		<center><br/>
+		<h2><span class="label label-danger">Follow This On</span></h2>
+		<br/><br/>
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+    			<div class="col-md-12">
+                    <ul class="social-network social-circle">
+                        <li><a href="#" class="icoFacebook si" title="Facebook"><i class="fa fa-facebook"></i></a>&nbsp;&nbsp;&nbsp;</li>
+                        <li><a href="#" class="icoTwitter si" title="Twitter"><i class="fa fa-twitter"></i></a>&nbsp;&nbsp;&nbsp;</li>
+                        <li><a href="#" class="icoGoogle si" title="Google +"><i class="fa fa-google-plus"></i></a>&nbsp;&nbsp;&nbsp;</li>
+                        <li><a href="#" class="icoLinkedin si" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>				
+				</div>
+ 
+		</center>
+		</div>
+		<br><br><br>
 	</div>
 </div>
+<br><br>
+
+<style>
+#tn {
+	border: 1px outset gray;
+}
+#spn
+	{
+	font-size:17px;
+}
+
+
+
+/*=========================
+  Icons
+ ================= */
+
+/* footer social icons */
+ul.social-network {
+	list-style: none;
+	display: inline;
+	margin-left:0 !important;
+	padding: 0;
+}
+ul.social-network li {
+	display: inline;
+	margin: 0 5px;
+}
+
+
+/* footer social icons */
+.social-network a.icoRss:hover {
+	background-color: #F56505;
+}
+.social-network a.icoFacebook:hover {
+	background-color:#3B5998;
+}
+.social-network a.icoTwitter:hover {
+	background-color:#33ccff;
+}
+.social-network a.icoGoogle:hover {
+	background-color:#BD3518;
+}
+.social-network a.icoVimeo:hover {
+	background-color:#0590B8;
+}
+.social-network a.icoLinkedin:hover {
+	background-color:#007bb7;
+}
+.social-network a.icoRss:hover i, .social-network a.icoFacebook:hover i, .social-network a.icoTwitter:hover i,
+.social-network a.icoGoogle:hover i, .social-network a.icoVimeo:hover i, .social-network a.icoLinkedin:hover i {
+	color:#fff;
+}
+a.socialIcon:hover, .socialHoverClass {
+	color:#44BCDD;
+}
+
+.social-circle li a {
+	display:inline-block;
+	position:relative;
+	margin:0 auto 0 auto;
+	-moz-border-radius:50%;
+	-webkit-border-radius:50%;
+	border-radius:50%;
+	text-align:center;
+	width: 50px;
+	height: 50px;
+	font-size:20px;
+}
+.social-circle li i {
+	margin:0;
+	line-height:50px;
+	text-align: center;
+}
+
+.social-circle li a:hover i, .triggeredHover {
+	-moz-transform: rotate(360deg);
+	-webkit-transform: rotate(360deg);
+	-ms--transform: rotate(360deg);
+	transform: rotate(360deg);
+	-webkit-transition: all 0.2s;
+	-moz-transition: all 0.2s;
+	-o-transition: all 0.2s;
+	-ms-transition: all 0.2s;
+	transition: all 0.2s;
+}
+.social-circle i {
+	color: #fff;
+	-webkit-transition: all 0.8s;
+	-moz-transition: all 0.8s;
+	-o-transition: all 0.8s;
+	-ms-transition: all 0.8s;
+	transition: all 0.8s;
+}
+
+.si {
+ background-color: #A0A0A0;   
+}
+
+</style>
