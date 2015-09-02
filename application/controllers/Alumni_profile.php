20f6c7ec377a449887780df257b70ee39bc6e61d
varun-garg <varun.10@live.com>
2015-09-02 03:46:25 +0000

    Update profile (basic)

--- application/controllers/Alumni_profile.php
+++ application/controllers/Alumni_profile.php
@@ -2,14 +2,7 @@
 
 class Alumni_profile extends CI_Controller {
 
-    function create() {
-        if($this->session->userdata('loggedin')!=1)
-            redirect ('/users?redirect=' . current_url ());
-        
-        $check_q = $this->db->query("select count(*) as profiles from alumni_basic where user_id = '".$this->session->userdata('user_id')."'");
-        $check = $check_q->row();
-        if($check->profiles >=1 ) redirect ('alumni_profile');
-        
+    function basic_form_1() {
         $this->load->helper('form');
         $this->load->helper('url');
         $this->load->library('form_validation');
@@ -20,6 +13,19 @@ class Alumni_profile extends CI_Controller {
         $this->form_validation->set_rules('marital_status', 'Marital status', 'required|max_length[100]');
 
         $this->form_validation->set_error_delimiters('<br /><span class="error">', '</span>');
+    }
+
+    function create() {
+        if ($this->session->userdata('loggedin') != 1)
+            redirect('/users?redirect=' . current_url());
+
+        $check_q = $this->db->query("select count(*) as profiles from alumni_basic where user_id = '" . $this->session->userdata('user_id') . "'");
+        $check = $check_q->row();
+        if ($check->profiles >= 1)
+            redirect('alumni_profile');
+
+        $this->basic_form_1();
+
 
         if ($this->form_validation->run() == FALSE) { // validation hasn't been passed
             $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
@@ -39,17 +45,54 @@ class Alumni_profile extends CI_Controller {
                 'marital_status' => set_value('marital_status')
             );
 
-            
-            if ($this->SaveForm($form_data) == TRUE) { // the information has therefore been successfully saved in the db
-                redirect('Alumni_profile/success');   // or whatever logic needs to occur
-            } else {
-                echo 'An error occurred saving your information. Please try again later';
-                // Or whatever error handling is necessary
-            }
+
+            $this->insert_basic($form_data);
+            redirect('Alumni_profile/success');
         }
     }
 
-    function SaveForm($form_data) {
+    function update() {
+        if ($this->session->userdata('loggedin') != 1)
+            redirect('/users?redirect=' . current_url());
+
+        $check_q = $this->db->query("select count(*) as profiles from alumni_basic where user_id = '" . $this->session->userdata('user_id') . "'");
+        $check = $check_q->row();
+        if ($check->profiles == 0)
+            redirect('alumni_profile/create');
+
+        $this->basic_form_1();
+
+        if ($this->form_validation->run() == FALSE) { // validation hasn't been passed
+            set_value('dob', '333');
+            $data['title'] = 'Alumni &nbsp;|&nbsp;  GBU Online';
+            $data['heading'] = ' GBU Alumni ';
+            $data['message'] = 'Let the world know you.....';
+            $this->load->view('pages/common/link', $data);
+            $this->load->view('pages/common/header');
+            $this->load->view('pages/common/page-heading', $data);
+
+            $query = $this->db->get_where('alumni_basic', array('user_id' => $this->session->userdata('user_id')));
+
+            $form_data = $query->row();
+
+            $this->load->view('pages/alumni/create_profile', $form_data);
+            $this->load->view('pages/common/footer');
+        } else {
+            $form_data = array(
+                'user_id' => $this->session->userdata('user_id'),
+                'dob' => set_value('dob'),
+                'address' => set_value('address'),
+                'gender' => set_value('gender'),
+                'marital_status' => set_value('marital_status')
+            );
+
+
+            $this->update_basic($form_data);
+            redirect('Alumni_profile/update');
+        }
+    }
+
+    function insert_basic($form_data) {
         $this->db->insert('alumni_basic', $form_data);
 
         if ($this->db->affected_rows() == '1') {
@@ -57,6 +100,11 @@ class Alumni_profile extends CI_Controller {
         }
 
         return FALSE;
+    }
+
+    function update_basic($form_data) {
+
+        $this->db->update('alumni_basic', $form_data, "user_id = '" . $form_data['user_id'] . "'");
     }
 
     function view() {
--- application/views/pages/alumni/create_profile.php
+++ application/views/pages/alumni/create_profile.php
@@ -14,19 +14,19 @@
         <br> 
         <link rel="stylesheet" href="<?php echo base_url() . 'users/application/views/common/' . 'dist/css/bootstrap-select.css' ?>">
         <script type="text/javascript" src="<?php echo base_url() . 'users/application/views/common/' . 'dist/js/bootstrap-select.js' ?>"></script>
-
-        <?php echo form_open('Alumni_profile/create'); ?>
+        <h3>Create/Update your profile </h3>
+        <?php echo form_open(current_url()); ?>
 
         <p>
             <label for="dob">Date Of birth <span class="required">*</span></label>
             <?php echo form_error('dob'); ?>
-            <br /><input class="form-control" id="dob" type="text" name="dob" maxlength="100" value="<?php echo set_value('dob'); ?>"  />
+            <br /><input class="form-control" id="dob" type="text" name="dob" maxlength="100" value="<?php echo set_value('dob',@$dob); ?>"  />
         </p>
 
         <p>
             <label for="address">Address <span class="required">*</span></label>
             <?php echo form_error('address'); ?>
-            <br /><input class="form-control" id="address" type="text" name="address"  value="<?php echo set_value('address'); ?>"  />
+            <br /><input class="form-control" id="address" type="text" name="address"  value="<?php echo set_value('address',@$address); ?>"  />
         </p>
 
         <p>
@@ -42,14 +42,13 @@
             );
             ?>
 
-            <br /><?php echo form_dropdown('gender', $options, set_value('gender'), 'class="selectpicker"') ?>
+            <br /><?php echo form_dropdown('gender', $options, set_value('gender',@$gender), 'class="selectpicker"') ?>
         </p>                                             
 
         <p>
             <label for="marital_status">Marital status <span class="required">*</span></label>
             <?php echo form_error('marital_status'); ?>
 
-            <?php // Change the values in this array to populate your dropdown as required ?>
             <?php
             $options = array(
                 '' => 'Please Select',
@@ -58,7 +57,7 @@
             );
             ?>
 
-            <br /><?php echo form_dropdown('marital_status', $options, set_value('marital_status'), 'class="selectpicker"') ?>
+            <br /><?php echo form_dropdown('marital_status', $options, set_value('marital_status',@$marital_status), 'class="selectpicker"') ?>
         </p>                                             
 
 
