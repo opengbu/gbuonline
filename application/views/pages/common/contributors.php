614a1c3ef842a8a2d4df3af56cbf819c68044b75
varun-garg <varun.10@live.com>
2015-09-16 02:14:32 +0000

    (Front End), added apurva to contributers

--- application/views/pages/common/contributors.php
+++ application/views/pages/common/contributors.php
@@ -10,40 +10,43 @@
         <i>
             <div class="row">
                 <div class="col-sm-4">
-                    <img src="<?php echo base_url('resources/images/varun.jpg') ?>" height ="120" width="120"><br><b>VARUN GARG </b><br/>(Admin/User Panel)<br />(+Database connectivity)<br />(Technical Head)
+                    <?php
+                    if (file_exists('resources/user_uploads/profile_images/1.jpg')) {
+                        ?>
+                        <img src="<?php echo base_url('resources/user_uploads/profile_images/1.jpg') ?>" width="120"><br>
+
+                        <?php
+                    } else {
+                        ?>
+                        <img src="<?php echo base_url('resources/images/varun.jpg') ?>" width="120">
+
+                        <?php
+                    }
+                    ?>
+                    <br><b>VARUN GARG </b><br/>(Admin/User Panel)<br />(with database connectivity)<br />(Technical Head)
                 </div>
                 <div class="col-sm-4">
-                    <img src="<?php echo base_url('resources/images/rajat.jpg') ?>" height ="120" width="120"><br><b>RAJAT SAXENA</b> <br/>(Front End Developer)<br>
+                    <img src="<?php echo base_url('resources/images/rajat.jpg') ?>" style="max-height: 170px;" width="120"><br><b>RAJAT SAXENA</b> <br/>(Front End Developer)<br>
                 </div>
                 <div class="col-sm-4">
-                    <img src="<?php echo base_url('resources/images/sarthak.jpg') ?>" height ="120" width="120"><br><b>SARTHAK GARG</b><br/> (Front End Developer)<br>
+                    <img src="<?php echo base_url('resources/images/sarthak.jpg') ?>" style="max-height: 170px;" width="120"><br><b>SARTHAK GARG</b><br/> (Front End Developer)<br>
                 </div>
             </div>
             <hr>
             <div class="row">
                 <div class="col-sm-4">
-                    <img src="<?php echo base_url('resources/images/shobhit.jpg') ?>" height ="120" width="120"><br><b>SHOBHIT SHARMA</b><br/> (Hostel Management System)
+                    <img src="<?php echo base_url('resources/images/shobhit.jpg') ?>"  style="max-height: 170px;" width="120"><br><b>SHOBHIT SHARMA</b><br/> (Hostel Management System)
                 </div>
                 <div class="col-sm-4">
-                    <img src="<?php echo base_url('resources/images/bhawesh.jpg') ?>" height ="120" width="120"><br><b>BHAWESH CHANDOLA</b><br/> (Front End Database Connectivity)
+                    <img src="<?php echo base_url('resources/images/bhawesh.jpg') ?>"  style="max-height: 170px;" width="120"><br><b>BHAWESH CHANDOLA</b><br/> (Front End Database Connectivity)
                 </div>
                 <div class="col-sm-4">
-                    <img src="<?php echo base_url('resources/images/rishabh.jpg') ?>" height ="120" width="120"><br><b>RISHABH AHUJA</b><br/>(Front End Developer)
+                    <img src="<?php echo base_url('resources/images/rishabh.jpg') ?>" style="max-height: 170px;" width="120"><br><b>RISHABH AHUJA</b><br/>(Front End Developer)
                 </div>
             </div>
-
-
         </i>      
 
     </ul>
-    <!--<hr>
-                                        <h3>Marketing Team</h3>
-                                        <hr>
-                                         <div class="row">
-                <center>
-                    <img src="<?php echo base_url('resources/images/shrey.jpg') ?>" height ="120" width="120"><br><b>SHREY KAPOOR </b><br/>(Marketing Head/Site Ambassador)
-               </center>
-                </div> -->
 </center>
 <hr>
 <center>
@@ -51,18 +54,34 @@
 </center>
 <hr>
 <div class="row">
-    <div class = "col-sm-6">
-        <center>
-            <img src="<?php echo base_url('resources/images/kshitij.jpg') ?>" height ="120" width="120"><br><b>KSHITIJ PURWAR</b><br/>(Content Manager)
-        </center>
-    </div>
-    <div class="col-sm-6">
-        <center>
-            <img src="<?php echo base_url('resources/images/vasudev.jpg') ?>" height ="120" width="120"><br><b>VASUDEV YADAV</b><br/>
-        </center>
-    </div>
+    <ul>
+        <div class = "col-sm-4">
+            <center>
+                <img src="<?php echo base_url('resources/images/kshitij.jpg') ?>"  width="120" style="max-height: 170px;"><br><b>KSHITIJ PURWAR</b><br/>(Content Head)
+            </center>
+        </div>
+        <div class="col-sm-4">
+            <center>
+                <?php
+                if (file_exists('resources/user_uploads/profile_images/38.jpg')) {
+                    ?>
+                    <img src="<?php echo base_url('resources/user_uploads/profile_images/38.jpg') ?>" width="120"><br>
+                    <?php
+                } else {
+                    ?>
+                    <img src="<?php echo base_url('resources/images/apurva.jpg') ?>" width="120">
+                    <?php
+                }
+                ?><br><b>APURVA JHA</b><br/>(Content Writer)
+            </center>
+        </div>
+        <div class="col-sm-4">
+            <center>
+                <img src="<?php echo base_url('resources/images/vasudev.jpg') ?>"  width="120" style="max-height: 170px;"><br><b>VASUDEV YADAV</b><br/>(Content Writer)
+            </center>
+        </div>
+        <br>
 
-
-    <br>
+    </ul>
 </div>
 <br/><br/><br/>
