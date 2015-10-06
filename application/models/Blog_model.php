7a788da1e08a92653f38f6430c1b39d1b8337386
Rajat Saxena <rajat.saxena035@gmail.com>
2015-10-06 19:46:34 +0530

    XSS vulnerability solved
    used CI's html_escape() , which is equivalent to htmlspecialchars()

--- application/models/Blog_model.php
+++ application/models/Blog_model.php
@@ -4,17 +4,17 @@ class Blog_model extends CI_Model {
 
     function insert_into_db() {
         $user_id = $this->session->userdata('user_id');
-        $title = $_POST['title'];
+        $title = html_escape($_POST['title']);
         $description = $_POST['description'];
-        $this->db->query("INSERT INTO blog (title,description,user_id) VALUES('$title','".$this->db->escape_str($description)."',$user_id)");
+        $this->db->query("INSERT INTO blog (title,description,user_id) VALUES('".$this->db->escape_str($title)."','".$this->db->escape_str($description)."',$user_id)");
     }
 	
 	function edit_into_db() {
         $user_id = $this->session->userdata('user_id');
-        $title = $_POST['title'];
+        $title = html_escape($_POST['title']);
         $description = $_POST['description'];
 		$bid = $_POST['bid'];
-        $this->db->query("UPDATE blog SET title = '$title', description = '".$this->db->escape_str($description)."' ,user_id = '$user_id', status = '3' WHERE id = '$bid'; ");
+        $this->db->query("UPDATE blog SET title = '".$this->db->escape_str($title)."', description = '".$this->db->escape_str($description)."' ,user_id = '$user_id', status = '3' WHERE id = '$bid'; ");
     }
 
 }
