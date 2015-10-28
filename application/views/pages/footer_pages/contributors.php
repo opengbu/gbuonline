9e7137d0b19f11a59fcfc27242aee1101fac8be2
Rajat Saxena <rajat.saxena035@gmail.com>
2015-10-28 22:57:49 +0530

    Technical Contributers  from GitHub
    Currently a bit slow, will improve it tomorrow

--- application/views/pages/footer_pages/contributors.php
+++ application/views/pages/footer_pages/contributors.php
@@ -1,52 +1,50 @@
+<script>
+var xmlhttp = new XMLHttpRequest();
+var url = "https://api.github.com/repos/opengbu/gbuonline/contributors";
+
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var myArr = JSON.parse(xmlhttp.responseText);
+        myFunction(myArr);
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += '<div class="col-sm-4"><img src="' + arr[i].avatar_url + '" width="120" style="height: 150px;"/><br/><b>' + arr[i].login + '</b><br/><i>Contributions: ' + arr[i].contributions + '</i><br/><br/><br/></div>';
+    }
+    document.getElementById("tech").innerHTML = out;
+}
+</script>
+
 <center> 
     <hr>
-    We are a group of students working under guidance of <br><b>Dr. Amit K. Awasthi.</b> <hr>
+    We are a group of students working under guidance of <br><b>Dr. Amit K. Awasthi.</b>
 </center>
-<center>   
 
-    <h3>Technical Team</h3>				
-    <hr>				
-    <ul>
-        <i>
-            <div class="row">
-                 <div class="col-sm-4">
-                    <img src="<?php echo $this->cdn->res_url('resources/images/bhawesh.jpg') ?>"  style="max-height: 170px;" width="120"><br><b>BHAWESH CHANDOLA</b><br/> (Front End Database Connectivity)
-                </div>
-                <div class="col-sm-4">
-                    <img src="<?php echo $this->cdn->res_url('resources/images/rajat.jpg') ?>" style="max-height: 170px;" width="120"><br><b>RAJAT SAXENA</b> <br/>(Front End Developer)<br>
-                </div>
-                <div class="col-sm-4">
-                    <img src="<?php echo $this->cdn->res_url('resources/images/rishabh.jpg') ?>" style="max-height: 170px;" width="120"><br><b>RISHABH AHUJA</b><br/>(Front End Developer)
-                </div>
-            </div>
-            <hr>
-            <div class="row">
-                <div class="col-sm-4">
-                    <img src="<?php echo $this->cdn->res_url('resources/images/sarthak.jpg') ?>" style="max-height: 170px;" width="120"><br><b>SARTHAK GARG</b><br/> (Front End Developer)<br>
-                </div>
-                <div class="col-sm-4">
-                    <img src="<?php echo $this->cdn->res_url('resources/images/shobhit.jpg') ?>"  style="max-height: 170px;" width="120"><br><b>SHOBHIT SHARMA</b><br/> (Hostel Management System)
-                </div>
-               <?php
-               { /*
-               NOTE, I am removing my name form this list only. It doesnot mean I have left this project
-                <div class="col-sm-4">
-                    <img src="<?php echo $this->cdn->res_url('resources/images/varun.jpg') ?>" width="120">
-                    <br><b>VARUN GARG </b><br/>(Admin/User Panel)<br />(Technical Head)
-                </div>
-                */ 
-               }
-               ?>
-            </div>
-        </i>      
+<div class="container-fluid">
+    <center>
+        <hr/>   
+        <h3>Technical Team</h3>				  
+        <hr/>				
+        <div id = "tech">
+            <b><i>Crunching the latest data, just for you. Hang tight.....</i></b>
+        </div>
+        <br/>
+        <hr/>
+    </center> 
+</div>
 
-    </ul>
-</center>
-<hr>
+<div class="container-fluid">
 <center>
+    <hr>
     <h3>Content Team</h3>
+    <hr>
 </center>
-<hr>
 <?php
 $content = $this->db->query("SELECT * FROM users WHERE type='cw' OR type = 'cm' ORDER BY type");
 foreach ($content->result() as $row) {
@@ -72,5 +70,5 @@ foreach ($content->result() as $row) {
 ?>
 <br/><br/>
 <hr>
-
+</div>
 
