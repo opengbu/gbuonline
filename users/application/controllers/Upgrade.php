82bbbe484ed89744850e089ae1c0e0147150054f
Rajat Saxena <rajat.saxena035@gmail.com>
2015-11-05 23:36:42 +0530

    Important DB query.

--- users/application/controllers/Upgrade.php
+++ users/application/controllers/Upgrade.php
@@ -2241,6 +2241,12 @@ class Upgrade extends CI_Controller {
 		array_push($update_list, $u);
         unset($u);
 
+        $u = new update;
+        $u->version = 5.8;
+        array_push($u->updates, "DELETE FROM blog_likes WHERE user_id = '0';");
+        array_push($update_list, $u);
+        unset($u);
+
         //Don't edit after this line
         $this->run_upgrades($update_list);
         redirect("login" . "?" . $_SERVER['QUERY_STRING']);
