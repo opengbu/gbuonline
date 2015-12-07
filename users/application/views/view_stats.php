9b6d60152de73f89e2416c9da376691300620b8d
Shobhit Sharma <shobhit281295@gmail.com>
2015-12-07 18:55:46 +0530

    Average hits added

--- users/application/views/view_stats.php
+++ users/application/views/view_stats.php
@@ -6,18 +6,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /* created by Rajat and Shobhit
 /* -->
 <div class = "container">
+    <!--Total Hits-->
     <br/><label style="font-size:20px;margin-left:30px;">Total Hits :</label>   
     <?php
     $hits_q = $this->db->query("select count(*) as hits_count from analysis");
     $hits = $hits_q->row();
     echo "<font size='10px'>" . $hits->hits_count . "</font>";
     ?>
+    <!--Today's Hits-->
     <br/><label style="font-size:20px;margin-left:30px;">Today's Hits :</label>   
     <?php
     $date = date('Y-m-d');
     $x = $this->db->query("select count(*) as today from analysis where date='$date'");
     $day_hits = $x->row();
     echo "<font size='5'><b>" . $day_hits->today . "</b></font>";
-    ?>                     
+    ?>
+     <!--Per Day Average-->
+    <br/><label style="font-size:20px;margin-left:30px;">Average:</label>   
+    <?php
+    $x = $this->db->query("select count(distinct date) as avg from analysis");
+    $day_hits = $x->row();
+    echo "<font size='5'><b>" . $day_hits->avg . "</b></font>&nbsp;hits per day";
+    ?>
 </div>
 <!--Hits Ends-->
