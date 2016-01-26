ecc92272e46d45bb25a4ad4d08ccd2d16e19ed5c
Rajat Saxena <rajat.saxena035@gmail.com>
2016-01-26 21:53:28 +0530

    separated quotes from codes.

--- resources/quotes.js
+++ resources/quotes.js
@@ -1,5 +1,11 @@
-document.writeln('<b><font size=\'+1\'><a target=\'_blank\' class=\'tqpHeader\' href=\"http://www.quotationspage.com/mqotd.html\"></a></font></b><br>');
-document.writeln('<dl>');
-document.writeln('<dt class=\'tqpQuote\'>Wish not so much to live long as to live well.</dt>');
-document.writeln('<dd> <b><font size=\'-1\'><a class=\'tqpAuthor\' target=\'_blank\' href=\"http://www.quotationspage.com/quotes/Benjamin_Franklin\">Benjamin Franklin</a></font></b></dd><p> </p>');
-document.writeln('</dl>');
+func([
+		{"quote":"Some succeed because they are destined to, but most succeed because they are determined to.", "author":"Henry Van Dyke"},
+		{"quote":"Success is nothing more than a few simple disciplines, practiced every day.", "author":"Jim Rohn"},
+		{"quote":"Life is 10% what happens to you and 90% how you react to it.", "author":"Charles R. Swindoll"},
+		{"quote":"A real friend is one who walks in when the rest of the world walks out.", "author":"Walter Winchell"},
+		{"quote":"Don't watch the clock; do what it does. Keep going.", "author":"Sam Levenson"},
+		{"quote":"The purpose of education is to replace an empty mind with an open one.", "author":"Malcolm Forbes"},
+		{"quote":"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.", "author":"Thomas Edison"},
+		{"quote":"Give me six hours to chop down a tree and I will spend the first four sharpening the axe.", "author":"Abraham Lincoln"},
+		{"quote":"If you don’t build your dream, someone else will hire you to help them build theirs.", "author":"Dhirubhai Ambani"}
+	])
